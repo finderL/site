77bcafa221f234ee5914c87175feac8bdf940351
finderL <liubei0528@gmail.com>
2015-03-04 09:17:39 +0800

    add default null page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta content="user-scalable=no,minimum-scale=1,maximum-scale=1,width=device-width" name="viewport" />
+	<meta content="telephone=no" name="format-detection" />
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-title" content="finder">
+	<title>finder</title>
+	<link rel="shortcut icon" href="/favicon.ico" />
+</head>
+<body>
+	
+</body>
+</html>
